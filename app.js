488d9855b520f2fc6b7fdbab7d50544f507e1d7a
Abdulhameed Yunusa <abdulhameed179@outlook.com>
2023-02-09 19:16:51 +0000

    added the routes for the about and contact pages

--- app.js
+++ app.js
@@ -17,10 +17,17 @@ app.set('view engine', 'ejs')
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static('public'))
 
-app.get('/', (req, res) =>
-  [
-    res.render('home', { homeContent: homeStartingContent })
-  ])
+app.get('/', (req, res) => {
+  res.render('home', { homeContent: homeStartingContent })
+})
+
+app.get('/about', (req, res) => {
+  res.render('about', { aboutContent: aboutContent })
+})
+
+app.get('/contact', (req, res) => {
+  res.render('contact', { contactContent: contactContent })
+})
 
 app.listen(3000, function () {
   console.log('Server started on port 3000')
